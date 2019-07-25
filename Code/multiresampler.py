29eaaa540977a148980b7c57ae307144ac59ad96
Matthew Gonzalgo <matthew.matty.g@gmail.com>
2019-07-25 16:56:16 -0700

    Debugged with Ian

--- Code/multiresampler.py
+++ Code/multiresampler.py
@@ -60,6 +60,8 @@ def get_variables(annotation):
     # Convert the list of "strings" into floats
     print("Raw input variables:")
     print(variables)
+    print('\n')
+
     variables = list(map(float, variables)) 
 
     vardict =	{
@@ -71,18 +73,19 @@ def get_variables(annotation):
     'lon_start' : variables[3],
     'lat_space' : variables[4],
     'lon_space' : variables[5],
-    # ul_lat : variables[6],
-    # ul_lon : variables[7],
+    'ul_lat' : variables[6],
+    'ul_lon' : variables[7],
     'ur_lat' : variables[8],
     'ur_lon' : variables[9],
     'll_lat' : variables[10],
     'll_lon' : variables[11],
-    # lr_lat : variables[12],
-    # lr_lon : variables[13],
+    'lr_lat' : variables[12],
+    'lr_lon' : variables[13],
     }
 
     print("Variable dictionary created:")
-    print(vardict.values())
+    print(vardict)
+    print('\n')
     
     return vardict  
 
@@ -91,47 +94,55 @@ def get_variables(annotation):
 # Plots both the original data input and the resampled data
 # May need to change saving locations on other machines
 #
-def save_and_show(original, xspace, yspace, resampled, resolution, name, path):
+def save_and_show(original, xspace, yspace, lats, lons, resampled, resolution, name, save_path):
     print("Saving original and resampled plots")
     start_time = time.time()
     plt.figure(figsize=(10,10));plt.imshow(original, vmin=0, vmax=500, cmap='jet');plt.colorbar()
     print("Original plotting time: --- %s seconds ---" % (time.time() - start_time))
-    plt.savefig(path + name + "original.png")
+    plt.savefig(save_path + name + "original.png")
+    print('Saved original')
+
+    start_time = time.time()
+    plt.figure(figsize=(10, 10));plt.pcolormesh(lons, lats, original);plt.colorbar();plt.grid()
+    print("Original latlon plotting time: --- %s seconds ---" % (time.time() - start_time))
+
+    plt.savefig(save_path + name + "original_latlon.png")
+    print('Saved original latlon')
 
     start_time = time.time()
     plt.figure(figsize=(10, 10));plt.pcolormesh(xspace, yspace, resampled);plt.colorbar();plt.grid()
     print("Resampled plotting time: --- %s seconds ---" % (time.time() - start_time))
+    plt.savefig(save_path + name + "resampled.png")
+    print('Saved resampled')
 
-    plt.savefig(path + name + "resampled.png")
-    print("Save complete")
+    print("Save complete\n")
     
     # Show plots for the purpose of checking
     # plt.show()
     
 
 
+def make_latlon_edges(lat_start, lon_start, lat_space, lon_space, lat_lines, lon_lines):
+    # Area def uses edges
+    lat_edges = np.linspace(lat_start - 0.5 * lat_space, (lat_start - 0.5 * lat_space) + (lat_space * (lat_lines + 1)), lat_lines + 1)
+    lon_edges = np.linspace(lon_start - 0.5 * lon_space, (lon_start - 0.5 * lon_space) + (lon_space * (lon_lines + 1)), lon_lines + 1)
 
-def resample(grid, vardict, area_new, test, resolution):
+    return lat_edges, lon_edges
+
+def make_latlon_centers(lat_start, lon_start, lat_space, lon_space, lat_lines, lon_lines):
+    # Area def uses edges
+    lat_centers = np.linspace(lat_start, lat_start + (lat_space * (lat_lines)), lat_lines)
+    lon_centers = np.linspace(lon_start, lon_start + (lon_space * (lon_lines )), lon_lines)
+
+    return lat_centers, lon_centers
+
+def resample(grid, annotation, vardict, lat_edges_b, lon_edges_l, lat_edges_t, lon_edges_r, area_new, test, resolution):
     print("Grid shape:")
     print(grid.shape)
 
-    # Assuming data and annotation file have the same name, just different type and contain only one period
-    lats = np.linspace(vardict['lat_start'] - 0.5 * vardict['lat_space'], (vardict['lat_start'] - 0.5 * vardict['lat_space']) + (vardict['lat_space'] * (vardict['lat_lines'] + 1)), vardict['lat_lines'] + 1)
-    lons = np.linspace(vardict['lon_start'] - 0.5 * vardict['lon_space'], (vardict['lon_start'] - 0.5 * vardict['lon_space']) + (vardict['lon_space'] * (vardict['lon_lines'] + 1)), vardict['lon_lines'] + 1)
 
-    # Option to downside for testing purposes
-    grid = grid[::test, ::test]
-    lats = lats[::test]
-    lons = lons[::test]
-    lon_lines = lons.size
-    lat_lines = lats.size
-
-    # Original area size and grid size match for test = 100, 
-    # but original area is one greater in both dimensions for test = 1 
-    # Correcting for that difference,
-    if test == 1: # there may be more cases
-        lon_lines = lon_lines - 1
-        lat_lines = lat_lines - 1
+    num_grid_rows = grid.shape[0]
+    num_grid_columns = grid.shape[1]
 
     #
     # Original Area definition:
@@ -140,9 +151,16 @@ def resample(grid, vardict, area_new, test, resolution):
     description = 'lat-lon'
     proj_id = annotation[annotation.find("gr"):annotation.find(".")]
     proj_string = 'EPSG:4326'
-    width = lon_lines
-    height = lat_lines
-    area_extent = (vardict['ll_lon'], vardict['ll_lat'], vardict['ur_lon'], vardict['ur_lat'])
+    width = num_grid_columns
+    height = num_grid_rows 
+
+
+    # Hack to deal with two cases: west/east on first column 
+    #if vardict['ll_lon'] > vardict['ur_lon']:
+    area_extent = (lon_edges_l[0], lat_edges_b[0], lon_edges_r[-1], lat_edges_t[-1])
+    #else:   
+    #    area_extent = (vardict['ll_lon'], vardict['ur_lat'], vardict['ur_lon'], vardict['ll_lat'])
+        
     area_original = geometry.AreaDefinition(area_id, description, proj_id, proj_string, width, height, area_extent)
 
  
@@ -170,25 +188,35 @@ def resample(grid, vardict, area_new, test, resolution):
     ye = np.linspace(y_high, y_low, height_new)
     '''
     
-    print("Resample complete")
+    print("Resample complete\n")
     return result
 
+def downsize(grid, lat_edges, lon_edges, lat_centers, lon_centers, test):
+    lat_centers = lat_centers[::test]
+    lon_centers = lon_centers[::test]
+    lat_edges_b = lat_edges[::test]
+    lon_edges_l = lon_edges[::test]
+    lat_edges_t = lat_edges[1::test]
+    lon_edges_r = lon_edges[1::test]
+ 
+    grid = grid[::test, ::test]
+    return grid, lat_centers, lon_centers, lat_edges_b, lon_edges_l, lat_edges_t, lon_edges_r
 
 #
 # Gets the max and min values in x and y directions for utm 
 # as well as the change in both x and y.
 # Returns the information as a tuple
 #
-def get_utm_range(ll_lat, ll_lon, ur_lat, ur_lon):
-    x_low = utm.from_latlon(ll_lat, ll_lon)[0]
-    x_high = utm.from_latlon(ur_lat, ur_lon)[0]
-    dx = x_high - x_low
+def get_utm_range(max_lat, max_lon, min_lat, min_lon):
+    min_x = utm.from_latlon(min_lat, min_lon)[0]
+    max_x = utm.from_latlon(max_lat, max_lon)[0]
+    dx = abs(max_x - min_x) # Shouldn't have to be absolute value
 
-    y_low = utm.from_latlon(ll_lat, ll_lon)[1]
-    y_high = utm.from_latlon(ur_lat, ur_lon)[1]
-    dy = y_high - y_low
+    min_y = utm.from_latlon(min_lat, min_lon)[1]
+    max_y = utm.from_latlon(max_lat, max_lon)[1]
+    dy = abs(max_y - min_y) # Shouldn't have to be absolute value
 
-    return (x_low, x_high, dx, y_low, y_high, dy)
+    return (min_x, max_x, dx, min_y, max_y, dy)
 
 
 # 
@@ -207,18 +235,36 @@ def scale_dimensions(factor, x_low, x_high, y_low, y_high, dx, dy):
 # 
 # Creates utm coordinate arrays to use for saving with the xarray Dataset
 #
-def create_utms(resolution, ll_lat, ll_lon, ur_lat, ur_lon):
- 
-    (x_low, x_high, dx, y_low, y_high, dy) = get_utm_range(ll_lat, ll_lon, ur_lat, ur_lon)
-    (x_lower, x_higher, y_lower, y_higher) = scale_dimensions(.2, x_low, x_high, y_low, y_high, dx, dy)
-    
+def create_utms(resolution, max_lat, max_lon, min_lat, min_lon):
+    print("--- Creating utm area ---")
+
+    print('(resolution, max_lat, max_lon, min_lat, min_lon)')
+    print(resolution, max_lat, max_lon, min_lat, min_lon)
+
+    (min_x, max_x, dx, min_y, max_y, dy) = get_utm_range(max_lat, max_lon, min_lat, min_lon)
+    print('(min_x, max_x, dx, min_y, max_y, dy)')
+    print(min_x, max_x, dx, min_y, max_y, dy)
+
+    '''
+    x_low = min(ll_x, ur_x)
+    x_high = max(ll_x, ur_x)
+    y_low = min(ll_y, ur_y)
+    y_high = max(ll_y, ur_y)
+    '''
+    (x_lower, x_higher, y_lower, y_higher) = scale_dimensions(.1, min_x, max_x, min_y, max_y, dx, dy)
+    print('--- Scaled dimensions ---')
+    print('(x_lower, x_higher, y_lower, y_higher)')
+    print(x_lower, x_higher, y_lower, y_higher)
+
     # Simplifying dimensions 
     real_x_low = round(x_lower, -3)
     real_y_low = round(y_lower, -3)
+    print('Simplified dimensions')
+    print('Low:')
+    print(real_x_low, real_y_low)
 
-
-    Dx = x_higher - real_x_low
-    Dy = y_higher - real_y_low
+    Dx = abs(x_higher - real_x_low)
+    Dy = abs(y_higher - real_y_low)
 
 
     n_xcells = (math.ceil(Dx / resolution))
@@ -226,11 +272,14 @@ def create_utms(resolution, ll_lat, ll_lon, ur_lat, ur_lon):
 
     real_x_high = real_x_low + n_xcells * resolution
     real_y_high = real_y_low + n_ycells * resolution
-    
-    print(dx)
-    print(dy)
+    print('High:')
+    print(real_x_high, real_y_high)
+
+    print('Real Dx/Dy:')
     print(Dx)
     print(Dy)
+
+    print('Number of x/y cells:')
     print(n_xcells)
     print(n_ycells)
 
@@ -247,8 +296,9 @@ def create_utms(resolution, ll_lat, ll_lon, ur_lat, ur_lon):
     # x_utms = np.linspace(real_x_low, real_x_high, 76)
     # y_utms = np.linspace(real_y_low, real_y_high, 258)
 
+    print('x/y utm corner shapes')
     print(x_utm_corners.shape, y_utm_corners.shape)
-    print("utm arrays created")
+    print("utm arrays created\n")
     return (x_utm_corners, y_utm_corners, x_utm_centers, y_utm_centers, n_xcells, n_ycells)
 
 
@@ -257,7 +307,7 @@ def create_utms(resolution, ll_lat, ll_lon, ur_lat, ur_lon):
 # Requires the resampled data, utm arrays, and the new resampled area to save the swath as an xarray Dataset
 # The resolution and full path of the data file are used for 
 #
-def save_resample(resampled, utm_x, utm_y, new_area, resolution, name, path):
+def save_resample(resampled, utm_x, utm_y, new_area, resolution, name, save_path):
     new_grid_lon, new_grid_lat = new_area.get_lonlats()
     swath = xr.Dataset( \
     {'elevation': (['y','x'], resampled)}, \
@@ -324,9 +374,9 @@ def save_resample(resampled, utm_x, utm_y, new_area, resolution, name, path):
     
     print(swath)
     
-    saved = path + name + ".nc"
+    saved = save_path + name + ".nc"
     swath.to_netcdf(saved)
-    print("Swath saved to " + saved)
+    print("Swath saved to " + saved + "\n")
 
 #
 # Simply creates a directory with the given name
@@ -339,6 +389,41 @@ def create_directory(dirName):
     except FileExistsError:
         print("Directory " , dirName ,  " already exists")
 
+def max_area(directory):
+    # Values initially set to max and min to set up initial comparison 
+    max_area_dict =	{
+        'max_lat' : -float('inf'),
+        'max_lon' : -float('inf'),
+        'min_lat' : float('inf'),
+        'min_lon' : float('inf')
+        }
+    os.chdir(directory)
+    vardict_dict = {}
+    for annotation in glob.glob("*.ann"):
+        vardict = get_variables(annotation)
+        vardict_dict.update({annotation : vardict}) # Save vardicts for later going through each grid
+        varmax_lat = max(vardict['ur_lat'], vardict['ll_lat'], vardict['lr_lat'], vardict['ul_lat'])
+        varmax_lon = max(vardict['ur_lon'], vardict['ll_lon'], vardict['lr_lon'], vardict['ul_lon'])
+        varmin_lat = min(vardict['ur_lat'], vardict['ll_lat'], vardict['lr_lat'], vardict['ul_lat'])
+        varmin_lon = min(vardict['ur_lon'], vardict['ll_lon'], vardict['lr_lon'], vardict['ul_lon'])
+
+        if varmax_lat > max_area_dict['max_lat']:
+            max_area_dict['max_lat'] = varmax_lat
+
+        if varmax_lon > max_area_dict['max_lon']:
+            max_area_dict['max_lon'] = varmax_lon
+
+        if varmin_lat < max_area_dict['min_lat']:
+            max_area_dict['min_lat'] = varmin_lat
+
+        if varmin_lon < max_area_dict['min_lon']:
+            max_area_dict['min_lon'] = varmin_lon
+
+    print("Max corners from all years of swath data:")
+    print(max_area_dict)
+    print('\n')
+    return max_area_dict, vardict_dict
+
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -348,11 +433,11 @@ if __name__ == '__main__':
     # Format for adding an argument:
     # parser.add_argument("-x", "--fullname", action="store", help="comment", default=default value, dest="variable_name",  type=datatype, required=T/F)
     #
-    parser.add_argument("-d", "--directory", action="store", help="Complete path to directory", dest="dir", type=str, required=True)
+    parser.add_argument("-i", "--input", action="store", help="Complete path to input directory", dest="dir", type=str, required=True)
     parser.add_argument("-t", "--test", action="store", help="Varible to scale down data and decrease runtime for testing", default=1, dest="test", type=int, required=False)
     parser.add_argument("-r", "--res", action="store", help="Resolution", default=10, dest="res", type=int, required=False)
     parser.add_argument("-x", "--suffix", action="store", help="filename suffix", default="", dest="suf",  type=str, required=False)
-
+    parser.add_argument("-o", "--output", action="store", help="Complete path to output directory", dest="save_dir",  type=str, required=True)
     args = parser.parse_args()
     
     # Indicate test runs in their save name
@@ -374,40 +459,16 @@ if __name__ == '__main__':
     assert args.res > 0, "resolution must be greater than 0"
 
 
-    # Values initially set to max and min to set up initial comparison 
-    cornerdict =	{
-        'ur_lat' : -float('inf'),
-        'ur_lon' : -float('inf'),
-        'll_lat' : float('inf'),
-        'll_lon' : float('inf')
-        }
-    os.chdir(args.dir)
-    vardict_dict = {}
-    for annotation in glob.glob("*.ann"):
-        vardict = get_variables(annotation)
-        vardict_dict.update({annotation : vardict}) # Save vardicts for later going through each grid
-        if vardict['ur_lat'] > cornerdict['ur_lat']:
-            cornerdict['ur_lat'] = vardict['ur_lat']
-
-        if vardict['ur_lon'] > cornerdict['ur_lon']:
-            cornerdict['ur_lon'] = vardict['ur_lon']
-
-        if vardict['ll_lat'] < cornerdict['ll_lat']:
-            cornerdict['ll_lat'] = vardict['ll_lat']
-
-        if vardict['ll_lon'] < cornerdict['ll_lon']:
-            cornerdict['ll_lon'] = vardict['ll_lon']
-    print(cornerdict)
-
-    print("--- Creating utm area ---")
+    max_area_dict, vardict_dict = max_area(args.dir)
+    
     (x_utm_corners, y_utm_corners, x_utm_centers, y_utm_centers, n_xcells, n_ycells) = create_utms(args.res, \
-                                                                                        cornerdict['ll_lat'], \
-                                                                                        cornerdict['ll_lon'], \
-                                                                                        cornerdict['ur_lat'], \
-                                                                                        cornerdict['ur_lon'])
-    print("New utm area created")
+                                                                                        max_area_dict['max_lat'], \
+                                                                                        max_area_dict['max_lon'], \
+                                                                                        max_area_dict['min_lat'], \
+                                                                                        max_area_dict['min_lon'])
 
-    tag = str(utm.from_latlon(cornerdict['ll_lat'], cornerdict['ll_lon'])[2])
+
+    tag = str(utm.from_latlon(max_area_dict['max_lat'], max_area_dict['min_lon'])[2])
     print("Tag: " + tag)
 
     # Gets name of directory
@@ -427,7 +488,10 @@ if __name__ == '__main__':
     print(width_new)
     print(height_new)
 
-    area_extent_new = (x_utm_corners[0], y_utm_corners[-1], x_utm_centers[-1], y_utm_corners[0]) # may need to revisit
+    print('(x_utm_corners[0], y_utm_corners[0], x_utm_corners[-1], y_utm_corners[-1])')
+    print(x_utm_corners[0], y_utm_corners[0], x_utm_corners[-1], y_utm_corners[-1])
+
+    area_extent_new = (x_utm_corners[0], y_utm_corners[0], x_utm_corners[-1], y_utm_corners[-1]) # may need to revisit
     area_new = geometry.AreaDefinition(area_id_new, description_new, proj_id_new, proj_string_new, width_new, height_new, area_extent_new)
 
     print("New area shape:")
@@ -455,26 +519,38 @@ if __name__ == '__main__':
         grid_nan = np.where(grid > grid.min(), grid, np.nan)
 
 
-        print("Annotation:" + annotation)
+        print("Annotation:" + ann)
         n = grd[grd.rfind('/') + 1:grd.find(".")]
         print("Name: " + n)
         name = str(args.res) + "m_" + n + args.suf
         print("Modified name: " + name)
 
+        
+    
+        lat_edges, lon_edges = make_latlon_edges(vdict['lat_start'], vdict['lon_start'], vdict['lat_space'], vdict['lon_space'], vdict['lat_lines'], vdict['lon_lines'])
+        print('lat_edges[0], lon_edges[0], lat_edges[-1], lon_edges[-1]')
+        print(lat_edges[0], lon_edges[0], lat_edges[-1], lon_edges[-1])
+        
+        lat_centers, lon_centers = make_latlon_centers(vdict['lat_start'], vdict['lon_start'], vdict['lat_space'], vdict['lon_space'], vdict['lat_lines'], vdict['lon_lines'])
+        #grid_nan, lat_centers, lon_centers = downsize(grid_nan, lat_centers, lat_centers, args.test)
+        grid_nan, lat_centers, lon_centers, lat_edges_b, lon_edges_l, lat_edges_t, lon_edges_r = \
+            downsize(grid_nan, lat_edges, lon_edges, lat_centers, lon_centers, args.test)
+        plot = resample(grid_nan, ann, vdict, lat_edges_b, lon_edges_l, lat_edges_t, lon_edges_r, area_new, args.test, args.res)
+
+
         # Creating and saving this new directory within the current directory of data
-        fig_dir = args.dir + args.dir[args.dir.rfind("/"):] + "_plots"
+        fig_dir = args.save_dir + args.dir[args.dir.rfind("/"):] + "_plots"
         print("fig_dir: " + fig_dir)
         create_directory(fig_dir)
-
-        plot = resample(grid_nan, vdict, area_new, args.test, args.res)
 
         # For the purpose of saving,
         fig_dir = fig_dir + "/"
         print("fig_dir/: " + fig_dir)
-        save_and_show(grid_nan, xx, yy, plot, args.res, name, fig_dir)
+        
+        save_and_show(grid_nan, xx, yy, lat_centers, lon_centers, plot, args.res, name, fig_dir)
 
         # Also creating and saving this new directory within the current directory
-        netCDF_dir = args.dir + args.dir[args.dir.rfind("/"):] + "_netCDF"
+        netCDF_dir = args.save_dir + args.dir[args.dir.rfind("/"):] + "_netCDF"
         print("netCDF_dir/: " + netCDF_dir)
         create_directory(netCDF_dir)
         
@@ -487,4 +563,4 @@ if __name__ == '__main__':
 
 # save_resample(plot, x_utm_centers, y_utm_centers, new_area, args.res, name, args.save)
     print("Total runtime of multiresampler: --- %s seconds ---" % (time.time() - start_time))
-    print("Done!")
+    print("Done!\n")
